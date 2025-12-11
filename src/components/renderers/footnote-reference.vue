ae217616d4b11f03eadba0afc06945f2ebdc7ca5
jinghaihan <jhh19980114@gmail.com>
2025-12-12 00:18:38 +0800

    feat: add footnote reference

---
+++ src/components/renderers/footnote-reference.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { FootnoteReferenceNodeRendererProps } from '../../types'
+import { computed } from 'vue'
+import { useContext } from '../../composables'
+
+const props = withDefaults(defineProps<FootnoteReferenceNodeRendererProps>(), {})
+
+const { getContainer } = useContext()
+
+const id = computed(() => props.node.identifier)
+const label = computed(() => props.node.label ?? id.value)
+
+function scrollToFootnote() {
+  const container = getContainer() || document.body
+  const footnoteElement = container.querySelector(`#footnote-${id.value}`)
+  if (!footnoteElement)
+    return
+  footnoteElement.scrollIntoView({ behavior: 'smooth' })
+}
+</script>
+
+<template>
+  <sup data-stream-markdown="footnote-reference" @click="scrollToFootnote">
+    <a :id="`footnote-reference-${id}`">[{{ label }}]</a>
+  </sup>
+</template>
