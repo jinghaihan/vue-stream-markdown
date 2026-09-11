1e2db1ba4f6f549c84926774357046c0c41d004d
jinghaihan <jhh19980114@gmail.com>
2026-09-11 20:28:02 +0800

    fix(vue): refine modern code block header

--- packages/vue/src/components/code-block/variants/modern.vue
+++ packages/vue/src/components/code-block/variants/modern.vue
@@ -10,14 +10,14 @@ defineProps<CodeBlockVariantProps>()
     data-variant="modern"
     dir="ltr"
     :data-collapsed="collapsed"
-    class="my-4 p-0.5 border border-border/70 rounded-2xl bg-muted/80 overflow-clip"
+    class="my-4 p-0.5 border border-border/70 rounded-2xl bg-[color-mix(in_srgb,var(--muted)_80%,var(--background)_20%)] overflow-clip"
     :class="[
       { 'code-loading': loading },
     ]"
   >
     <header
       data-stream-markdown="code-block-header"
-      class="code-block-header text-sm text-muted-foreground px-3 py-1.5 pr-2.5 flex gap-2 items-center top-0 justify-between sticky z-[5] max-lg:px-2 [&>*:last-child]:flex [&>*:first-child]:flex-1 [&>*:last-child]:shrink-0 [&>*:first-child]:min-w-0 [&>*:last-child]:items-center [&>*:nth-child(2)]:left-1/2 [&>*:nth-child(2)]:absolute [&>*:nth-child(2)]:-translate-x-1/2"
+      class="code-block-header text-sm text-muted-foreground px-3 py-1 pr-2.5 bg-[color-mix(in_srgb,var(--muted)_80%,var(--background)_20%)] flex gap-2 items-center top-0 justify-between sticky z-[5] max-lg:px-2 [&>*:last-child]:flex [&>*:first-child]:flex-1 [&>*:last-child]:shrink-0 [&>*:first-child]:min-w-0 [&>*:last-child]:items-center [&>*:nth-child(2)]:left-1/2 [&>*:nth-child(2)]:absolute [&>*:nth-child(2)]:-translate-x-1/2"
     >
       <slot name="title" />
       <slot name="header-center" />
